f2d823575606e507b07a3843124e05b2f68d346c
Stephen Coyle <steviecoyle@hotmail.com>
2025-02-25 22:13:37 +0000

    initial springboot code commit

---
+++ src/main/java/com/scoyle/media_vault/MediaVaultApplication.java
@@ -0,0 +1,13 @@
+package com.scoyle.media_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaVaultApplication.class, args);
+	}
+
+}
