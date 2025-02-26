aece53acb45a049dc4644e53d2626afe4d3f8b7a
Stephen Coyle <steviecoyle@hotmail.com>
2025-02-26 00:07:12 +0000

    add empty BooksController

---
+++ src/main/java/com/scoyle/media_vault/controller/BooksController.java
@@ -0,0 +1,13 @@
+package com.scoyle.media_vault.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/books")
+public class BooksController {
+}
