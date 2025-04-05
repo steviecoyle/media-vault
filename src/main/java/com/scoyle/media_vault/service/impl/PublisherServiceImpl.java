f0a719e96c8ae8fc80baf30ff82e05b2fa84ef43
Stephen Coyle <steviecoyle@hotmail.com>
2025-04-05 22:49:07 +0100

    add PublisherService and impl

---
+++ src/main/java/com/scoyle/media_vault/service/impl/PublisherServiceImpl.java
@@ -0,0 +1,44 @@
+package com.scoyle.media_vault.service.impl;
+
+import com.scoyle.media_vault.persistence.entity.PublishersEntity;
+import com.scoyle.media_vault.persistence.repository.PublisherRepository;
+import com.scoyle.media_vault.request.CreatePublisherRequest;
+import com.scoyle.media_vault.request.UpdatePublisherRequest;
+import com.scoyle.media_vault.service.PublisherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+class PublisherServiceImpl implements PublisherService {
+
+    private final PublisherRepository repository;
+
+    @Override
+    public PublishersEntity savePublisher(CreatePublisherRequest request) {
+        PublishersEntity entity = new PublishersEntity();
+        entity.setName(request.getName());
+
+        PublishersEntity publisher = repository.save(entity);
+        return publisher;
+    }
+
+    @Override
+    public PublishersEntity updatePublisher(UpdatePublisherRequest updatePublisherRequest) {
+        Optional<PublishersEntity> optionalPublisher = repository.findById(updatePublisherRequest.getId());
+
+        if (optionalPublisher.isPresent()) {
+            PublishersEntity publisherToUpdate = optionalPublisher.get();
+            publisherToUpdate.setName(updatePublisherRequest.getName());
+            publisherToUpdate.setLastUpdated(null);
+
+            PublishersEntity updatedPublisher = repository.save(publisherToUpdate);
+
+            return updatedPublisher;
+        } else {
+            return null;
+        }
+    }
+}
