8767aac34b2e05c0b71cea7591390c8ace691411
Stephen Coyle <steviecoyle@hotmail.com>
2025-07-29 22:10:04 +0100

    finish REST calls for publisher content

--- src/main/java/com/scoyle/media_vault/service/PublisherService.java
+++ src/main/java/com/scoyle/media_vault/service/PublisherService.java
@@ -3,10 +3,14 @@ package com.scoyle.media_vault.service;
 import com.scoyle.media_vault.persistence.entity.PublishersEntity;
 import com.scoyle.media_vault.request.CreatePublisherRequest;
 import com.scoyle.media_vault.request.UpdatePublisherRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PublisherService {
 
+    Page<PublishersEntity> getAllPublishers(Pageable pageable);
+
     PublishersEntity savePublisher(CreatePublisherRequest request);
 
-    PublishersEntity updatePublisher(UpdatePublisherRequest updatePublisherRequest);
+    PublishersEntity updatePublisher(String id, UpdatePublisherRequest updatePublisherRequest);
 }
--- src/main/java/com/scoyle/media_vault/service/impl/PublisherServiceImpl.java
+++ src/main/java/com/scoyle/media_vault/service/impl/PublisherServiceImpl.java
@@ -1,20 +1,31 @@
 package com.scoyle.media_vault.service.impl;
 
+import com.scoyle.media_vault.exception.ResourceNotFoundException;
 import com.scoyle.media_vault.persistence.entity.PublishersEntity;
 import com.scoyle.media_vault.persistence.repository.PublisherRepository;
 import com.scoyle.media_vault.request.CreatePublisherRequest;
 import com.scoyle.media_vault.request.UpdatePublisherRequest;
 import com.scoyle.media_vault.service.PublisherService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 class PublisherServiceImpl implements PublisherService {
 
     private final PublisherRepository repository;
+
+    @Override
+    public Page<PublishersEntity> getAllPublishers(Pageable pageable) {
+
+        return repository.findAll(pageable);
+    }
 
     @Override
     public PublishersEntity savePublisher(CreatePublisherRequest request) {
@@ -25,17 +36,25 @@ class PublisherServiceImpl implements PublisherService {
     }
 
     @Override
-    public PublishersEntity updatePublisher(UpdatePublisherRequest updatePublisherRequest) {
-        Optional<PublishersEntity> optionalPublisher = repository.findById(updatePublisherRequest.getId());
+    public PublishersEntity updatePublisher(String id, UpdatePublisherRequest updatePublisherRequest) {
+        try {
+            Long publisherId = Long.parseLong(id);
+            Optional<PublishersEntity> optionalPublisher = repository.findById(publisherId);
 
-        if (optionalPublisher.isPresent()) {
+            if (optionalPublisher.isEmpty()) {
+                log.error("Publisher with Id [{}] not found", publisherId);
+                throw new ResourceNotFoundException("Publisher with Id [" + publisherId + "] not found");
+            }
+
             PublishersEntity publisherToUpdate = optionalPublisher.get();
             publisherToUpdate.setName(updatePublisherRequest.getName());
             publisherToUpdate.setLastUpdated(null);
 
             return repository.save(publisherToUpdate);
-        } else {
-            return null;
+
+        } catch (NumberFormatException nfe) {
+            log.error("Invalid Publisher Id - {}", nfe.getMessage());
+            throw new IllegalArgumentException("Invalid Publisher Id");
         }
     }
 }
