c9b828c5eddbe755dd10a1fc2312f58d2b1de059
Stephen Coyle <steviecoyle@hotmail.com>
2026-03-23 13:14:53 +0000

    add some tests to GamesControllerTest

--- src/test/java/com/scoyle/media_vault/integration/controller/GamesControllerTest.java
+++ src/test/java/com/scoyle/media_vault/integration/controller/GamesControllerTest.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,14 +28,34 @@ class GamesControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void whenInputIsInvalid_thenReturnsStatus400() throws Exception {
+    void givenEmptyRequest_thenReturnStatus400() throws Exception {
         CreateGameRequest request = new CreateGameRequest();
         String body = objectMapper.writeValueAsString(request);
 
-        mvc.perform(post("/games")
+        MvcResult result = mvc.perform(post("/games")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(body))
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest()).andReturn();
+
+        System.out.println("Result: " + result.getResponse().getContentAsString());
+
+        MockHttpServletResponse resp = result.getResponse();
+        assertEquals(400, resp.getStatus());
+    }
+
+    @Test
+    void givenEmptyRequestBody_thenReturnStatus400() throws Exception {
+
+        MvcResult result = mvc.perform(post("/games")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""))
+                .andDo(print())
+                .andExpect(status().isBadRequest()).andReturn();
+
+        System.out.println("Result: " + result.getResponse().getContentAsString());
+
+        MockHttpServletResponse resp = result.getResponse();
+        assertEquals(400, resp.getStatus());
     }
 }
